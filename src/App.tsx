ce67ed65a5e7f15cdbd41542873d1e2e45ff3a76
Disabledbarrel <elin.elisabeth.larsson@gmail.com>
2023-05-19 21:50:35 +0200

    counter with redux and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSelector } from "react-redux";
+import "./App.css";
+import Button from "./components/Button";
+import { RootState } from "./redux/store";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { count } = useSelector((state: RootState) => state.counter);
+	return (
+		<div className="App">
+			<h1>Elin's awesome counter</h1>
+			<p>The count is: {count}</p>
+			<Button text="Increment" action="increment" />
+			<Button text="Decrement" action="decrement" />
+		</div>
+	);
 }
 
 export default App;
